6d03d6134d1997c41ead756197e8872a2e15fe17
akshay8281 <akshaypitroda4@gmail.com>
2023-08-06 23:21:50 +0530

    Meter-Feet

---
+++ Meter-to-feet.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Float into Meter
+
+int main()
+ {
+
+float meter,feet;
+
+printf("Enter the feet : \n");
+scanf("%f",&feet);
+
+meter = feet / 3.2808399;
+printf("meter : %f\n",meter);
+
+  return 0;
+}
